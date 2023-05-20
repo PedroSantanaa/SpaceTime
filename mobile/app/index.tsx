c72e262858ceeb3db0659b04d68213d46e3b317e
Pedro Santana <pedro.ferraz01@gmail.com>
2023-05-19 22:01:16 -0300

    feat:mobile auth with github

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -6,19 +6,57 @@ import {
   Roboto_700Bold as Roboto700,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold as BaiJamjuree700 } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from './src/assets/bg-blue.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlwMobileLogo.svg'
+import blurBg from '../src/assets/bg-blue.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlwMobileLogo.svg'
 import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useEffect } from 'react'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
+import { useRouter } from 'expo-router'
 
 const StylesStripes = styled(Stripes)
 
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/<93782f4c7cc6e817a57a>',
+}
+
 export default function App() {
+  const router = useRouter()
   const [hasLoadedFonts] = useFonts({
     Roboto400,
     Roboto700,
     BaiJamjuree700,
   })
+  const [, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '93782f4c7cc6e817a57a',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacestime',
+      }),
+    },
+    discovery,
+  )
+  async function HandlerOAuthGithub(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+    router.push('/memories')
+  }
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      HandlerOAuthGithub(code)
+    }
+  }, [response])
   if (!hasLoadedFonts) {
     return null
   }
@@ -43,6 +81,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.8}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => promptAsync()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             {' '}
